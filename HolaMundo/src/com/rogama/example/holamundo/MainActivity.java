4e3411fd739a4fc43db17c3eef7a00262ba1af34
rogama <rogama_87@hotmail.com>
2013-03-03 20:51:27 +0100

    Definir un elemento de interfac por codigo

--- HolaMundo/src/com/rogama/example/holamundo/MainActivity.java
+++ HolaMundo/src/com/rogama/example/holamundo/MainActivity.java
@@ -3,13 +3,22 @@ package com.rogama.example.holamundo;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        
+        TextView texto2 = new TextView(this);
+        texto2.setText("Hola!!, Android");
+        texto2.setPadding(0, 50, 0, 0);
+        texto2.setHeight(10);
+        texto2.setId(640);
+
+        setContentView(texto2);
     }
 
 
