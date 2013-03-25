cb95c100d1f08d34396c9a4c040013a71d536cd2
rogama <rogama_87@hotmail.com>
2013-03-25 22:10:51 +0100

    Lanzar una segunda ventana mediante propiedad OnClick

---
+++ Lanzar2ventana/src/com/rogama/lanzar2ventana/MainActivity.java
@@ -0,0 +1,29 @@
+package com.rogama.lanzar2ventana;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void lanzar2ventana(View v) {
+		Intent inten = new Intent(this,ventana2.class);
+		startActivity(inten);
+	}
+
+}
