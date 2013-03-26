b78d3fc5f9b6951bc5317765d6871f4f8fb0d1f0
rogama <rogama_87@hotmail.com>
2013-03-26 22:18:32 +0100

    Lanzando Activity por codigo

--- Lanzar2ventana/src/com/rogama/lanzar2ventana/MainActivity.java
+++ Lanzar2ventana/src/com/rogama/lanzar2ventana/MainActivity.java
@@ -5,13 +5,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	private Button BtnTerceraventana; 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		BtnTerceraventana = (Button) findViewById(R.id.button2);
+		BtnTerceraventana.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View view) {
+				lanzarVentanaPorCodigo(null);
+			}
+		});
 	}
 
 	@Override
@@ -22,8 +33,12 @@ public class MainActivity extends Activity {
 	}
 	
 	public void lanzar2ventana(View v) {
-		Intent inten = new Intent(this,ventana2.class);
+		Intent inten = new Intent(this, ventana2.class);
 		startActivity(inten);
 	}
-
+	
+	public void lanzarVentanaPorCodigo(View v) {
+		Intent inten = new Intent(this, abriendoPorCodigo.class);
+		startActivity(inten);
+	}
 }
---
+++ Lanzar2ventana/src/com/rogama/lanzar2ventana/abriendoPorCodigo.java
@@ -0,0 +1,15 @@
+package com.rogama.lanzar2ventana;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class abriendoPorCodigo extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.abriendoporcodigo);
+	}
+
+}
