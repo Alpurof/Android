978f73e97ae2f911d2dcafb835eed9a8379b5c2d
rogama <rogama_87@hotmail.com>
2013-04-05 21:06:39 +0200

    configurado para que podamos arrastrar las vistas creadas personalmente a un layout (esto queda comentado en main.xml)

--- EjemploGraficos/src/com/rogama/ejemplograficos/EjemploView5.java
+++ EjemploGraficos/src/com/rogama/ejemplograficos/EjemploView5.java
@@ -1,24 +1,33 @@
 package com.rogama.ejemplograficos;
 
+import java.text.AttributedCharacterIterator.Attribute;
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
-import android.os.Bundle;
+import android.util.AttributeSet;
 import android.view.View;
 
 public class EjemploView5 extends View {
 	private ShapeDrawable miImagen;
-	public EjemploView5(Context context) {
-		super(context);
+	
+	public EjemploView5(Context context, AttributeSet attrs) {
+		super(context,attrs);
 		miImagen = new ShapeDrawable(new OvalShape());
 		miImagen.getPaint().setColor(0xff0000ff);
-		miImagen.setBounds(10,10,310,60);
+		
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
 		miImagen.draw(canvas);
+	}
+
+	@Override
+	protected void onSizeChanged(int ancho, int alto, int oldw, int oldh) {
+		// TODO Auto-generated method stub
+		miImagen.setBounds(0,0,ancho,alto);
 	}
 
 }
