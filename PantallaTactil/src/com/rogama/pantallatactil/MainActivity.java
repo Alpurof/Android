a08803a032e12886314cf2fa3ca8406da252c71e
rogama <rogama_87@hotmail.com>
2013-04-06 23:20:27 +0200

    Programa de ejemplo que muestra los eventos de las pulsaciones

---
+++ PantallaTactil/src/com/rogama/pantallatactil/MainActivity.java
@@ -0,0 +1,37 @@
+package com.rogama.pantallatactil;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnTouchListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		TextView entrada = (TextView) findViewById(R.id.textViewEntrada);
+		entrada.setOnTouchListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onTouch(View vista, MotionEvent evento) {
+		TextView salida = (TextView) findViewById(R.id.textViewSalida);
+		salida.append(evento.toString()+"\n");
+		
+		return true;
+	}
+
+}
